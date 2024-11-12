4a8f0a96ce1983ce5da2e5ee0c26c5609903e99f
Elise-oyi <maryeliseangbo@gmail.com>
2024-11-12 12:57:47 +0000

    added a handler and removed app.ts file

--- api/graphql/handler.ts
+++ api/graphql/handler.ts
@@ -1,8 +1,8 @@
-import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { DatabaseService } from "./services/database";
-import { typeDefs } from "./graphql/schema";
-import { resolvers } from "./graphql/resolvers";
+import express from "express";
+import { DatabaseService } from "../../src/services/database";
+import { typeDefs } from "../../src/graphql/schema";
+import { resolvers } from "../../src/graphql/resolvers";
 
 const startServer = async () => {
   const app = express();
@@ -16,18 +16,14 @@ const startServer = async () => {
   });
 
   await server.start();
-  server.applyMiddleware({ app, path: "/graphql" });
-
-  app.use(express.json());
+  server.applyMiddleware({ app, path: "/api/graphql" });
 
   return app;
 };
 
-const serverPromise = startServer();
+let serverPromise = startServer();
 
 export default async function handler(req: any, res: any) {
   const app = await serverPromise;
   app(req, res);
 }
-
-
